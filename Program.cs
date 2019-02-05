54a656d37e1109b0fd6fa18b968b058fb1a98eb7
endlesstravel <yx33355555@163.com>
2019-02-05 14:59:10 +0800

    tmp save

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FlexCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 111;
+            change(ref a);
+            Console.WriteLine("Hello World!", a);
+        }
+
+        static void change(ref int v)
+        {
+            v = 12;
+        }
+    }
+}
