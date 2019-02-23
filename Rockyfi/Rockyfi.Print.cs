f60bb85fc01e2b0473434c3712a3070c698aaba0
endlesstravel <yx33355555@163.com>
2019-02-23 17:08:25 +0800

    add StringToXXX

--- Rockyfi/Rockyfi.Print.cs
+++ Rockyfi/Rockyfi.Print.cs
@@ -192,8 +192,7 @@ namespace Rockyfi
         }
 
 
-
-
+        #region XXXX_ToSring
         // AlignToString returns string version of Align enum
         public static string AlignToString(Align value)
         {
@@ -457,6 +456,226 @@ namespace Rockyfi
             }
             return "unknown";
         }
+        #endregion
+
+        #region StringTo_XXXX
+
+        // AlignToString returns string version of Align enum
+        public static bool StringToAlign(string value, out Align result)
+        {
+            switch (value)
+            {
+                case "auto": result = Align.Auto; return true;
+                case "flex-start": result = Align.FlexStart; return true;
+                case "center": result = Align.Center; return true;
+                case "flex-end": result = Align.FlexEnd; return true;
+                case "stretch": result = Align.Stretch; return true;
+                case "baseline": result = Align.Baseline; return true;
+                case "space-between": result = Align.SpaceBetween; return true;
+                case "space-around": result = Align.SpaceAround; return true;
+            }
+            result = Align.Auto;
+            return true;
+        }
+
+        // DimensionToString returns string version of Dimension enum
+        public static bool StringToDimension(string value, out Dimension result)
+        {
+            switch (value)
+            {
+                case "width": result = Dimension.Width; return true;
+                case "height": result = Dimension.Height; return true;
+            }
+            result = Dimension.Width;
+            return true;
+        }
+
+        // DirectionToString returns string version of Direction enum
+        public static bool StringToDirection(string value, out Direction result)
+        {
+            switch (value)
+            {
+                case "inherit": result = Direction.Inherit; return true;
+                case "ltr": result = Direction.LTR; return true;
+                case "rtl": result = Direction.RTL; return true;
+            }
+            result = Direction.Inherit;
+            return true;
+        }
+
+        // DisplayToString returns string version of Display enum
+        public static bool StringToDisplay(string value, out Display result)
+        {
+            switch (value)
+            {
+                case "flex": result = Display.Flex; return true;
+                case "none": result = Display.None; return true;
+            }
+            result = Display.Flex;
+            return true;
+        }
+
+        // EdgeToString returns string version of Edge enum
+        public static bool StringToEdge(string value, out Edge result)
+        {
+            switch (value)
+            {
+                case "left": result = Edge.Left; return true;
+                case "top": result = Edge.Top; return true;
+                case "right": result = Edge.Right; return true;
+                case "bottom": result = Edge.Bottom; return true;
+                case "start": result = Edge.Start; return true;
+                case "end": result = Edge.End; return true;
+                case "horizontal": result = Edge.Horizontal; return true;
+                case "vertical": result = Edge.Vertical; return true;
+                case "all": result = Edge.All; return true;
+            }
+            result = Edge.Left;
+            return true;
+        }
+
+        // ExperimentalFeatureToString returns string version of ExperimentalFeature enum
+        public static bool StringToExperimentalFeature(string value, out ExperimentalFeature result)
+        {
+            switch (value)
+            {
+                case "web-flex-basis": result = ExperimentalFeature.WebFlexBasis; return true;
+            }
+            result = ExperimentalFeature.WebFlexBasis;
+            return true;
+        }
+
+        // FlexDirectionToString returns string version of FlexDirection enum
+        public static bool StringToFlexDirection(string value, out FlexDirection result)
+        {
+            switch (value)
+            {
+                case "column": result = FlexDirection.Column; return true;
+                case "column-reverse": result = FlexDirection.ColumnReverse; return true;
+                case "row": result = FlexDirection.Row; return true;
+                case "row-reverse": result = FlexDirection.RowReverse; return true;
+            }
+            result = FlexDirection.Column;
+            return true;
+        }
+
+        // JustifyToString returns string version of Justify enum
+        public static bool StringToJustify(string value, out Justify result)
+        {
+            switch (value)
+            {
+                case "flex-start": result = Justify.FlexStart; return true;
+                case "center": result = Justify.Center; return true;
+                case "flex-end": result = Justify.FlexEnd; return true;
+                case "space-between": result = Justify.SpaceBetween; return true;
+                case "space-around": result = Justify.SpaceAround; return true;
+            }
+            result = Justify.FlexStart;
+            return true;
+        }
+
+        public static bool StringToLogLevel(string value, out LogLevel result)
+        {
+            switch (value)
+            {
+                case "error": result = LogLevel.Error; return true;
+                case "warn": result = LogLevel.Warn; return true;
+                case "info": result = LogLevel.Info; return true;
+                case "debug": result = LogLevel.Debug; return true;
+                case "verbose": result = LogLevel.Verbose; return true;
+                case "fatal": result = LogLevel.Fatal; return true;
+            }
+            result = LogLevel.Error;
+            return true;
+        }
+
+        public static bool StringToMeasureMode(string value, out MeasureMode result)
+        {
+            switch (value)
+            {
+                case "undefined": result = MeasureMode.Undefined; return true;
+                case "exactly": result = MeasureMode.Exactly; return true;
+                case "at-most": result = MeasureMode.AtMost; return true;
+            }
+            result = MeasureMode.Undefined;
+            return true;
+        }
+
+        public static bool StringToNodeType(string value, out NodeType result)
+        {
+            switch (value)
+            {
+                case "default": result = NodeType.Default; return true;
+                case "text": result = NodeType.Text; return true;
+            }
+            result = NodeType.Default;
+            return true;
+        }
+
+        public static bool StringToOverflow(string value, out Overflow result)
+        {
+            switch (value)
+            {
+                case "visible": result = Overflow.Visible; return true;
+                case "hidden": result = Overflow.Hidden; return true;
+                case "scroll": result = Overflow.Scroll; return true;
+            }
+            result = Overflow.Visible;
+            return true;
+        }
+
+        public static bool StringToPositionType(string value, out PositionType result)
+        {
+            switch (value)
+            {
+                case "relative": result = PositionType.Relative; return true;
+                case "absolute": result = PositionType.Absolute; return true;
+            }
+            result = PositionType.Relative;
+            return true;
+        }
+
+        // PrintOptionsToString returns string version of PrintOptions enum
+        public static bool StringToPrintOptions(string value, out PrintOptions result)
+        {
+            switch (value)
+            {
+                case "layout": result = PrintOptions.Layout; return true;
+                case "style": result = PrintOptions.Style; return true;
+                case "children": result = PrintOptions.Children; return true;
+            }
+            result = PrintOptions.Layout;
+            return true;
+        }
+
+        // UnitToString returns string version of Unit enum
+        public static bool StringToUnit(string value, out Unit result)
+        {
+            switch (value)
+            {
+                case "undefined": result = Unit.Undefined; return true;
+                case "point": result = Unit.Point; return true;
+                case "percent": result = Unit.Percent; return true;
+                case "auto": result = Unit.Auto; return true;
+            }
+            result = Unit.Undefined;
+            return true;
+        }
+
+        // WrapToString returns string version of Wrap enum
+        public static bool StringToWrap(string value, out Wrap result)
+        {
+            switch (value)
+            {
+                case "no-wrap": result = Wrap.NoWrap; return true;
+                case "wrap": result = Wrap.Wrap; return true;
+                case "wrap-reverse": result = Wrap.WrapReverse; return true;
+            }
+            result = Wrap.NoWrap;
+            return true;
+        }
+
+        #endregion
 
     }
 
